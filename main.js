4dbcc8c3e1a5e43d884cd9f6352893b4c6a6b438
Travis Wiley <traviswiley88@gmail.com>
2023-12-07 08:30:07 -0800

    Additions HTML CSS

--- main.js
+++ main.js
@@ -22,4 +22,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     // Append the footer content to the body
                     document.body.appendChild(...footerContainer.childNodes);
+
+
             });
+document.addEventListener("DOMContentLoaded", function () {
+            // Contact Form
+    $('#contact-form').submit(function (event) {
+        event.preventDefault(); 
+
+        const nameInput = $('#name');
+        const phoneInput = $('#phone');
+        const emailInput = $('#email-contact');
+        const contactMethodInput = $("input[name='contact-method']:checked");
+        const commentsInput = $('#comments');
+        const nameError = $('#name-error');
+        const phoneError = $('#phone-error');
+        const emailError = $('#email-error');
+        const contactMethodError = $('#contact-method-error');
+        const commentsError = $('#comments-error');
+
+        // Validate inputs
+        const isValidName = validateNotEmpty(nameInput.val());
+        const isValidPhone = validateNotEmpty(phoneInput.val());
+        const isValidEmail = validateEmail(emailInput.val());
+        const isValidContactMethod = contactMethodInput.length > 0;
+        const isValidComments = validateNotEmpty(commentsInput.val());
+
+        // Display error messages
+        displayErrorMessage(isValidName, nameError, 'Please enter your name.');
+        displayErrorMessage(isValidPhone, phoneError, 'Please enter your phone number.');
+        displayErrorMessage(isValidEmail, emailError, 'Please enter a valid email address.');
+        displayErrorMessage(isValidContactMethod, contactMethodError, 'Please select a contact method.');
+        displayErrorMessage(isValidComments, commentsError, 'Please enter your comments.');
+
+        // Check if all inputs are valid
+        if (isValidName && isValidPhone && isValidEmail && isValidContactMethod && isValidComments) {
+            // Handle successful submission (you can replace this with your own logic)
+            alert('Thank you for contacting us. We will reach out to you soon.');
+            // Clear the form
+            $('#contact-form')[0].reset();
+        }
+    });
